767af1e683856fb42867e139b60ce1b127bdfd0f
Vladimir <neolog13@gmail.com>
2025-03-07 21:58:54 +0400

    Simple search created

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.catalog_categories, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
     path('category/<slug:category_slug>/', views.catalog_products, name='category'),
+    path('search/', views.catalog_products, name='search'),
     # path('category/<slug:category_slug>/<int:page>', views.catalog_products, name='category'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
-# 7:36
+# 7:46
 
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from catalog.models import Categories, Products
+from catalog.utils import q_search
 
 
 def catalog_categories(request):
@@ -15,13 +16,19 @@ def catalog_categories(request):
     return render(request, 'catalog/catalog.html', context=context)
 
 
-def catalog_products(request, category_slug):
+def catalog_products(request, category_slug=None):
 
     page = request.GET.get('page', 1)
     order_by = request.GET.get('order_by', None)
+    
+    query = request.GET.get('q', None)
 
     categories = Categories.objects.all()
     products = Products.objects.filter(category__slug=category_slug)
+
+    if query:
+        products = q_search(query)
+
 
     if order_by and order_by != "default":
         products = products.order_by(order_by)
