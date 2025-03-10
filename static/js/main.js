c61b6db24f58b0ec95e48148aca5fac4838d5a7d
Vladimir <neolog13@gmail.com>
2025-03-10 20:58:17 +0400

    Profile form, controller & notifications are ready

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,3 @@
-// const tabItem = document.querySelectorAll('.tabs__btn-item');
-// const tabContent = document.querySelectorAll('.tabs__content-item');
-
-// tabItem.forEach(function (element) {
-//   element.addEventListener('click', open);
-// })
-
-// function open(evt) {
-//   const tabTarget = evt.currentTarget;
-//   const button = tabTarget.dataset.button;
-
-//   tabItem.forEach(function(item) {
-//     item.classList.remove('tabs__btn-item--active');
-//   });
-
-//   tabTarget.classList.add('tabs__btn-item--active');
-
-//   tabContent.forEach(function(item) {
-//     item.classList.remove('tabs__content-item--active');
-//   });
-
-//   const targetContent = document.querySelector(`#${button}`);
-//   targetContent.classList.add('tabs__content-item--active');
-
-
-
   // adaptive
   const menuBtn = document.querySelector('.menu__btn');
   const menu = document.querySelector('.menu__list');
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 # 9:14:11 - кнопка выйти
 # 9:16:41 - иконки
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 def login(request):
     if request.method == 'POST':
@@ -16,6 +17,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, You are logged in")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -35,9 +37,11 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"{user.username}, You have successfully registered and logged into your account")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
+
     context = {
         'title': 'Home - Registration',
         'form': form
@@ -45,13 +49,26 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
-        'title': 'Home - Profile'
+        'title': 'Home - Profile',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
 
+@login_required
 def logout(request):
+    messages.success(request, f"{request.user.username}, You have logged out")
     auth.logout(request)
     return redirect(reverse('main:index'))
